92a1b8dcb27b087e81423a4a4e699d2248d08608
pedryn777 <pedrosilvamar777@gmail.com>
2026-01-28 16:57:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const items = document.querySelectorAll('.reveal');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('active');
+    }
+  });
+}, { threshold: 0.15 });
+
+items.forEach(item => observer.observe(item));
